4d2e121d8c5bac8da258e1c6551c382da2c7c4dd
Augustocfq <augustocfq@gmail.com>
2023-04-29 19:34:10 -0300

    refactor: applied dart standard naming

--- lib/src/presentation/pages/home_page.dart
+++ lib/src/presentation/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import '../widgets/nav_bar.dart';
-import '../widgets/top_bar.dart';
+import '../widgets/nav_bar_widget.dart';
+import '../widgets/top_bar_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
